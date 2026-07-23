502df6d0213ace47a1591cedf9714cf1a5630f41
MIKECROND <3025780005@qq.com>
2026-07-23 10:06:47 +0800

    feat(application): 添加转盘模块相关配置与工程引用
    新增turntable虚拟文件夹并添加源码编译引用，补充完善转盘模块的配置宏定义与注释

--- Applications/turntable/turntable_conf.h
+++ Applications/turntable/turntable_conf.h
@@ -1,12 +1,29 @@
 #ifndef __TURNTABLE_CONF_H__
 #define __TURNTABLE_CONF_H__
 
-#define TURNABLE_INIT 39.0f
+/**
+ * @brief 转盘模块初始角度
+ */
+#define TURNABLE_INIT -10.0f
 
-#define TURNABLE_ID_0_ANGLE 3.0f
+/**
+ * @brief 转盘模块ID0角度
+ */
+#define TURNABLE_ID_0_ANGLE 26.0f
+
+/**
+ * @brief 转盘模块角度间隔
+ */
 #define TURNABLE_ANGLE 36.0f
+
+/**
+ * @brief 转盘模块关闭角度偏移量
+ */
 #define TURNABLE_CLOSE_OFFSET 18.0f
 
+/**
+ * @brief 转盘模块ID最大数量
+ */
 #define TURNABLE_ID_MAX 5
 
 float turntable_id[TURNABLE_ID_MAX] = {TURNABLE_ID_0_ANGLE,
